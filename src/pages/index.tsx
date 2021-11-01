ba5f638a29388deefec3eec1f56547b181f28dae
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-01 16:11:16 +0700

    Removed Button components from index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,11 @@
 import type { ReactElement } from 'react';
-import Button from '../components/button/button';
 
 import Layout from '../components/Layout';
 
 const Index = (): ReactElement => (
-  <div>
-    <div style={{ padding: '10px' }}>
-      <Button />
-    </div>
-    <div style={{ padding: '10px' }}>
-      <Button size='fixed' theme='bordered' />
-    </div>
-    <div style={{ padding: '10px' }}>
-      <Button size='fixed' theme='filled' />
-    </div>
-    <div style={{ padding: '10px', width: '200px' }}>
-      <Button size='small' theme='bordered' />
-    </div>
-    <div style={{ padding: '10px', width: '200px' }}>
-      <Button size='small' theme='filled' />
-    </div>
-    <div style={{ padding: '10px' }}>
-      <Button size='fixed' theme='rounded' />
-    </div>
-  </div>
+  <Layout title='landing page' pageClass='landing'>
+    <h1>hihe</h1>
+  </Layout>
 );
 
 export default Index;
