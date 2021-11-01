6600494ac78a46596c6a1fa1849ccd908e17f083
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-01 12:24:16 +0700

    Changed hover effects and fixed dimensions

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
 import type { ReactElement } from 'react';
+import Button from '../components/button/button';
 
 import Layout from '../components/Layout';
 
 const Index = (): ReactElement => (
-  <Layout title='landing page' pageClass='landing'>
-    <h1>hihe</h1>
-  </Layout>
+  <div>
+    <div style={{ padding: '10px' }}>
+      <Button />
+    </div>
+    <div style={{ padding: '10px' }}>
+      <Button size='fixed' theme='bordered' />
+    </div>
+    <div style={{ padding: '10px' }}>
+      <Button size='fixed' theme='filled' />
+    </div>
+    <div style={{ padding: '10px', width: '200px' }}>
+      <Button size='small' theme='bordered' />
+    </div>
+    <div style={{ padding: '10px', width: '200px' }}>
+      <Button size='small' theme='filled' />
+    </div>
+    <div style={{ padding: '10px' }}>
+      <Button size='fixed' theme='rounded' />
+    </div>
+  </div>
 );
 
 export default Index;
