e4f1749a28e6c03cc4bcbda2bbee3011796d71b7
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-05 11:24:49 +0700

    The position: fixed property has been added to the Header component and a subHeader has been added

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useCallback } from 'react';
 import Link from 'next/link';
 import Navbar from '../Navbar/Navbar';
 import styles from './Header.module.scss';
@@ -16,39 +16,43 @@ type HeaderProps = {
   navbarItems?: NavbarListItem[];
 };
 
-const Header = ({
-  logo,
-  user,
-  navbarItems,
-}: HeaderProps): React.ReactElement => {
+const Header = ({ logo, user, navbarItems }: HeaderProps): React.ReactElement => {
   const [hamburgerIsActive, setHamburger] = useState(false);
-  const handleBurgerClick = (): void => {
-    setHamburger(!hamburgerIsActive);
-  };
+  const handleBurgerClick = (): void => { setHamburger(!hamburgerIsActive); };
+
+  const [headerHeight, setHeaderHeight] = useState(0);
+  const headerRef = useCallback((node) => {
+    if (node !== null) {
+      setHeaderHeight(node.getBoundingClientRect().height);
+    }
+  }, []);
 
   let burgerClasses = styles.burger;
   burgerClasses += hamburgerIsActive ? ` ${styles.burgerActive}` : '';
 
   return (
     <header className={styles.header}>
-      <div className={styles.content}>
-        <Link href='/'>
-          <a>
-            <img className={styles.logo} src={logo} alt='logo' />
-          </a>
-        </Link>
-        <Navbar
-          items={navbarItems}
-          user={user}
-          navbarIsOpened={hamburgerIsActive}
-        />
-        <button
-          type='button'
-          className={burgerClasses}
-          onClick={handleBurgerClick}
-        >
-          <div className={styles.burgerImage} />
-        </button>
+      <div className={styles.subHeader} style={{ height: `${headerHeight}px` }} />
+      <div className={styles.body} ref={headerRef}>
+        <div className={styles.content}>
+          <Link href='/'>
+            <a>
+              <img className={styles.logo} src={logo} alt='logo' />
+            </a>
+          </Link>
+          <Navbar
+            items={navbarItems}
+            user={user}
+            navbarIsOpened={hamburgerIsActive}
+          />
+          <button
+            type='button'
+            className={burgerClasses}
+            onClick={handleBurgerClick}
+          >
+            <div className={styles.burgerImage} />
+          </button>
+        </div>
       </div>
     </header>
   );
