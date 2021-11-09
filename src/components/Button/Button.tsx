c50f909f0beafef015c8d3c14b29694c8f77f39e
vitaliy <bakalov.vitaliy@gmail.com>
2021-11-09 11:49:49 +0700

    Fixed merge conflicts

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,67 +1,36 @@
 import Link from 'next/link';
 import styles from './Button.module.scss';
+import { ButtonModifiers, ButtonProps } from './Types';
 
-type ButtonType = {
-  type?: 'button' | 'submit';
-  text?: string;
-  isDisabled?: boolean;
-  theme?: 'filled' | 'bordered' | 'rounded';
-  size?: 'small' | 'fixed';
-  link?: string;
-  onClick?: () => void;
-};
-type Modifiers = Record<string, string>;
+const Button = ({ type, text = 'click me', isDisabled = false,
+  theme, size, link, onClick }: ButtonProps): JSX.Element => {
 
-const Button = ({
-  type,
-  text = 'click me',
-  isDisabled = false,
-  theme,
-  size,
-  link,
-  onClick,
-}: ButtonType): JSX.Element => {
-  const themes: Modifiers = {
-    filled: styles.button_theme_filled,
-    bordered: styles.button_theme_bordered,
-    rounded: styles.button_theme_rounded,
-  };
-  const sizes: Modifiers = {
-    small: styles.button_size_small,
-    fixed: styles.button_size_fixed,
+  const themes: ButtonModifiers = {
+    filled: styles.buttonThemeFilled,
+    bordered: styles.buttonThemeBordered,
+    rounded: styles.buttonThemeRounded,
   };
 
-  const classes = [
-    styles.button,
-    themes[theme || ''],
-    sizes[size || ''],
-  ];
-  const classesString = classes.filter((classItem) => classItem).join(' ');
+  const sizes: ButtonModifiers = {
+    small: styles.buttonSizeSmall,
+    fixed: styles.buttonSizeFixed,
+  };
+
+  const classes = [ styles.button, themes[theme || ''], sizes[size || '' ]];
+  const classesString = classes.join(' ');
 
   const handleButtonClick = (): void => {
     if (onClick) {
       onClick();
     }
   };
-
-  if (link) {
-    return (
-      <Link href={link}>
-        <a className={classesString}>{text}</a>
-      </Link>
-    );
-  }
-
-  return (
-    <button
-      className={classesString}
-      type={type}
-      disabled={isDisabled}
-      onClick={handleButtonClick}
-    >
+  const button = link ? 
+    <Link href={link}><a className={classesString}>{text}</a></Link> :
+    <button className={classesString} type={type} disabled={isDisabled} onClick={handleButtonClick}>
       {text}
     </button>
-  );
+
+  return button;
 };
 
 export default Button;
