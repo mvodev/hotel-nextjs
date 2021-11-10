a664186ab7fce9fc25c559956ea5bf14777bd8f7
vitalij bakalov <bakalov.vitaliy@gmail.com>
2021-11-10 20:17:15 +0700

    Removed bar between additional photos

--- src/components/RoomPhotoGallery/RoomPhotoGallery.tsx
+++ src/components/RoomPhotoGallery/RoomPhotoGallery.tsx
@@ -1,16 +1,29 @@
 import styles from './RoomPhotoGallery.module.scss';
 import RoomPhotoGalleryProps from './Types';
 
-const RoomPhotoGallery = ({ mainPhoto, firstAdditionalPhoto,
-  secondAdditionalPhoto }: RoomPhotoGalleryProps): React.ReactElement => {
+const RoomPhotoGallery = ({
+  mainPhoto,
+  firstAdditionalPhoto,
+  secondAdditionalPhoto,
+}: RoomPhotoGalleryProps): React.ReactElement => {
   const roomPhotoGallery = (
     <div className={styles.roomPhotoGallery}>
-      <img className={styles.mainPhoto} src={`./images/${mainPhoto}`} alt='room' />
+      <img
+        className={styles.mainPhoto}
+        src={`./images/${mainPhoto}`}
+        alt='room'
+      />
       <div className={styles.additionalPhotosSection}>
-        <img className={styles.additionalPhoto} src={`./images/${firstAdditionalPhoto}`}
-          alt='room' />
-        <img className={styles.additionalPhoto} src={`./images/${secondAdditionalPhoto}`}
-          alt='room' />
+        <img
+          className={styles.additionalPhoto}
+          src={`./images/${firstAdditionalPhoto}`}
+          alt='room'
+        />
+        <img
+          className={styles.additionalPhoto}
+          src={`./images/${secondAdditionalPhoto}`}
+          alt='room'
+        />
       </div>
     </div>
   );
