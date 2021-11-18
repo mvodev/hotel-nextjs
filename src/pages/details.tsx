7d779f6479668a1bde6fe069bafdbd54357cd267
Vadim Manushin <vadimmanushin@yandex.ru>
2021-11-18 18:37:20 +0300

    details page draft

---
+++ src/pages/details.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react';
+import Layout from '../components/Layout';
+
+import styles from '../styles/pages/details.module.scss';
+
+const Details = (): ReactElement => (
+  <Layout title='room details' pageClass='details'>
+  </Layout>
+);
+
+export default Details;
