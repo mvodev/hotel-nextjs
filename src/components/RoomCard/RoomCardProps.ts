f1556cd553bb82baea3a06e50b1a527a6a6adaff
Rizarid <rizarid@mail.ru>
2021-11-21 03:35:31 +0500

    #36 changeed RoomCard styles

---
+++ src/components/RoomCard/RoomCardProps.ts
@@ -0,0 +1,198 @@
+import RoomCardProps from "./Types";
+
+const roomCardProps: RoomCardProps[] = [
+  {
+    infoSection: {
+      roomNumber: 888,
+      isLux: true,
+      rating: 5,
+      price: 9990,
+      reviewsCount: 145
+    },
+    link: '/Filters',
+    slides: [
+      'room-1.webp',
+      'room-4.webp',
+      'room-7.webp',
+      'room-3.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 840,
+      isLux: false,
+      rating: 4,
+      price: 9900,
+      reviewsCount: 65
+    },
+    link: '/Filters',
+    slides: [
+      'room-2.webp',
+      'room-8.webp',
+      'room-4.webp',
+      'room-5.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 980,
+      isLux: true,
+      rating: 3,
+      price: 8500,
+      reviewsCount: 35
+    },
+    link: '/Filters',
+    slides: [
+      'room-3.webp',
+      'room-9.webp',
+      'room-1.webp',
+      'room-7.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 856,
+      isLux: false,
+      rating: 5,
+      price: 7300,
+      reviewsCount: 19
+    },
+    link: '/Filters',
+    slides: [
+      'room-4.webp',
+      'room-8.webp',
+      'room-2.webp',
+      'room-11.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 740,
+      isLux: false,
+      rating: 4,
+      price: 6000,
+      reviewsCount: 44
+    },
+    link: '/Filters',
+    slides: [
+      'room-5.webp',
+      'room-1.webp',
+      'room-12.webp',
+      'room-7.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 982,
+      isLux: true,
+      rating: 3,
+      price: 5800,
+      reviewsCount: 56
+    },
+    link: '/Filters',
+    slides: [
+      'room-6.webp',
+      'room-2.webp',
+      'room-9.webp',
+      'room-11.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 678,
+      isLux: false,
+      rating: 5,
+      price: 5500,
+      reviewsCount: 45
+    },
+    link: '/Filters',
+    slides: [
+      'room-7.webp',
+      'room-2.webp',
+      'room-9.webp',
+      'room-10.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 450,
+      isLux: false,
+      rating: 4,
+      price: 5300,
+      reviewsCount: 39
+    },
+    link: '/Filters',
+    slides: [
+      'room-8.webp',
+      'room-1.webp',
+      'room-4.webp',
+      'room-11.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 350,
+      isLux: false,
+      rating: 3,
+      price: 5000,
+      reviewsCount: 77
+    },
+    link: '/Filters',
+    slides: [
+      'room-9.webp',
+      'room-3.webp',
+      'room-12.webp',
+      'room-6.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 666,
+      isLux: false,
+      rating: 5,
+      price: 5000,
+      reviewsCount: 25
+    },
+    link: '/Filters',
+    slides: [
+      'room-10.webp',
+      'room-4.webp',
+      'room-1.webp',
+      'room-8.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 444,
+      isLux: false,
+      rating: 3,
+      price: 5000,
+      reviewsCount: 15
+    },
+    link: '/Filters',
+    slides: [
+      'room-11.webp',
+      'room-6.webp',
+      'room-2.webp',
+      'room-7.webp',
+    ]
+  },
+  {
+    infoSection: {
+      roomNumber: 352,
+      isLux: false,
+      rating: 3,
+      price: 5000,
+      reviewsCount: 55
+    },
+    link: '/Filters',
+    slides: [
+      'room-12.webp',
+      'room-5.webp',
+      'room-1.webp',
+      'room-10.webp',
+    ]
+  },
+] 
+
+export default roomCardProps;
