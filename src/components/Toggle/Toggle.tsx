8ceb0989e1cefb6b1ca86628015f0ae91eac6f8f
Vadim Manushin <vadimmanushin@yandex.ru>
2021-11-24 17:13:54 +0300

    prettier applied

--- src/components/Toggle/Toggle.tsx
+++ src/components/Toggle/Toggle.tsx
@@ -2,16 +2,23 @@
 import ToggleProps from './Types';
 import styles from './Toggle.module.scss';
 
-const Toggle = ( { isChecked, description, onChangeCallback }: ToggleProps): JSX.Element => 
+const Toggle = ({
+  isChecked,
+  description,
+  onChangeCallback,
+}: ToggleProps): JSX.Element => (
   <div className={styles.toggleContainer}>
-    <label className={styles.toggleLabel} > {description}
-      <input 
-        type='checkbox' 
+    <label className={styles.toggleLabel}>
+      {description}
+      <input
+        type='checkbox'
         defaultChecked={isChecked}
         className={styles.toggleInput}
-        onChange={onChangeCallback}/>
-      <span className={styles.toggleSwitch}/>
+        onChange={onChangeCallback}
+      />
+      <span className={styles.toggleSwitch} />
     </label>
   </div>
+);
 
 export default Toggle;
