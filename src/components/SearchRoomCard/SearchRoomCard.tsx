e97f0ecf350c64103c0a968ef00b8fd14b3affcd
gamabuntat <schoshoev@yandex.ru>
2021-11-25 23:32:11 +0800

    addded final form

--- src/components/SearchRoomCard/SearchRoomCard.tsx
+++ src/components/SearchRoomCard/SearchRoomCard.tsx
@@ -1,3 +1,5 @@
+import { Form, Field } from 'react-final-form'
+
 import DropdownGuests from 'src/components/DropdownGuests/DropdownGuests';
 import DropdownGuestsDefaultProps from 'src/components/DropdownGuests/DropdownGuestsDefaultProps';
 
@@ -10,26 +12,26 @@ type RoomRateCardState = {
   dateDropdown: DateDropdownType;
 };
 
-const SearchRoomCard = ({ dateDropdown }: RoomRateCardState): JSX.Element => {
-  const searchRoomCard = (
-    <div className={styles.searchRoomCard}>
-      <h1 className={styles.title}>Найдём номера под ваши пожелания</h1>
-      <form className={styles.form}>
-        <div className={styles.formItem}>
-          <DateDropdown {...dateDropdown} />
-        </div>
-        <div className={styles.formItem}>
-          <h3 className={styles.formItemTitle}>гости</h3>
-          <DropdownGuests {...DropdownGuestsDefaultProps} />
-        </div>
-        <div className={styles.buttonContainer}>
-          <Button type='submit' theme='filled' text='подобрать номер' />
-        </div>
-      </form>
-    </div>
-  );
-
-  return searchRoomCard;
-};
+const SearchRoomCard = ({ dateDropdown }: RoomRateCardState): JSX.Element => (
+  <Form onSubmit={() => console.log('submit!')}>
+    {(props) => (
+      <div className={styles.searchRoomCard}>
+        <h1 className={styles.title}>Найдём номера под ваши пожелания</h1>
+        <form className={styles.form} onSubmit={props.handleSubmit}>
+          <div className={styles.formItem}>
+            <DateDropdown {...dateDropdown} />
+          </div>
+          <div className={styles.formItem}>
+            <h3 className={styles.formItemTitle}>гости</h3>
+            <DropdownGuests {...DropdownGuestsDefaultProps} />
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button type='submit' theme='filled' text='подобрать номер' />
+          </div>
+        </form>
+      </div>
+    )}
+  </Form>
+);
 
 export default SearchRoomCard;
