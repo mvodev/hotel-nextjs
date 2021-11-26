c55c697a017f7a055f89eab404d3e888b6157c1f
gamabuntat <schoshoev@yandex.ru>
2021-11-26 21:36:01 +0800

    date dropdown draft

--- src/components/DateDropdown/DateDropdown.tsx
+++ src/components/DateDropdown/DateDropdown.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { selectDates, setDates } from 'src/redux/Dates/DatesSlice';
 
 import DatePicker from '../DatePicker/DatePicker';
 import DateDropdownType from './Types';
@@ -6,12 +9,12 @@ import DateDropdownType from './Types';
 import style from './DateDropdown.module.sass';
 
 const DateDropdown = ({
-  initDate = [null, null],
   titles = ['прибытие', 'выезд'],
   modifier = 'double',
   isSmall = false
 }: DateDropdownType): JSX.Element => {
-  const [value, setValue] = useState(initDate);
+  const value = useSelector(selectDates)
+  const dispatch = useDispatch();
   const [isOpen, setIsOpen] = useState(false);
 
   const getValueForDoubleField = (date: Date): string => {
@@ -65,11 +68,11 @@ const DateDropdown = ({
     </div>
   );
 
-  const handleChangeDate = (dates: [Date, Date]): void => setValue(dates);
+  const handleChangeDate = (dates: [Date, Date]) => dispatch(setDates(dates));
 
   const handleControlPanelUsed = (buttonType: string): void => {
     if (buttonType === 'clean') {
-      setValue([null, null]);
+      dispatch(setDates([null, null]));
       setIsOpen(false);
     } else setIsOpen(false);
   };
