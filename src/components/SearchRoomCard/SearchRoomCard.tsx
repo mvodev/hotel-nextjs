6a30c68c553af5b786a86e3ce0d55e5ad26bb729
gamabuntat <schoshoev@yandex.ru>
2021-11-27 04:13:22 +0800

    added saga

--- src/components/SearchRoomCard/SearchRoomCard.tsx
+++ src/components/SearchRoomCard/SearchRoomCard.tsx
@@ -1,6 +1,8 @@
 import { Form } from 'react-final-form'
+import { useDispatch } from 'react-redux';
 
 import DropdownGuests from 'src/components/DropdownGuests/DropdownGuests';
+import { submit } from 'src/redux/Filters/FiltersSlice';
 
 import Button from '../Button/Button';
 import DateDropdown from '../DateDropdown/DateDropdown';
@@ -11,26 +13,30 @@ type RoomRateCardState = {
   dateDropdown: DateDropdownType;
 };
 
-const SearchRoomCard = ({ dateDropdown }: RoomRateCardState): JSX.Element => (
-  <Form onSubmit={() => console.log('hihe')}>
-    {({ handleSubmit }) => (
-      <div className={styles.searchRoomCard}>
-        <h1 className={styles.title}>Найдём номера под ваши пожелания</h1>
-        <form className={styles.form} onSubmit={handleSubmit}>
-          <div className={styles.formItem}>
-            <DateDropdown {...dateDropdown} />
-          </div>
-          <div className={styles.formItem}>
-            <h3 className={styles.formItemTitle}>гости</h3>
-            <DropdownGuests />
-          </div>
-          <div className={styles.buttonContainer}>
-            <Button type='submit' theme='filled' text='подобрать номер' />
-          </div>
-        </form>
-      </div>
-    )}
-  </Form>
-);
+const SearchRoomCard = ({ dateDropdown }: RoomRateCardState): JSX.Element => {
+  const dispatch = useDispatch();
+
+  return (
+    <Form onSubmit={() => dispatch(submit())}>
+      {({ handleSubmit }) => (
+        <div className={styles.searchRoomCard}>
+          <h1 className={styles.title}>Найдём номера под ваши пожелания</h1>
+          <form className={styles.form} onSubmit={handleSubmit}>
+            <div className={styles.formItem}>
+              <DateDropdown {...dateDropdown} />
+            </div>
+            <div className={styles.formItem}>
+              <h3 className={styles.formItemTitle}>гости</h3>
+              <DropdownGuests />
+            </div>
+            <div className={styles.buttonContainer}>
+              <Button type='submit' theme='filled' text='подобрать номер' />
+            </div>
+          </form>
+        </div>
+      )}
+    </Form>
+  )
+};
 
 export default SearchRoomCard;
