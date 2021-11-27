459b45442bee0a3f6a3c3675238afb73093b705f
vitaliy <bakalov.vitaliy@gmail.com>
2021-11-27 17:53:56 +0700

    Added Saga

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,17 @@
-import { createStore } from '@reduxjs/toolkit';
+import createSagaMiddleware from '@redux-saga/core';
+import { applyMiddleware, createStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
 import reducers from './reduces';
+import RootSaga from './RootSaga';
 
-const makeStore = () => createStore(reducers);
+const sagaMiddleware = createSagaMiddleware();
+
+const makeStore = () => {
+  const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+  sagaMiddleware.run(RootSaga);
+  return store;
+};
+
 const wrapper = createWrapper(makeStore, { debug: true });
 
 export default wrapper;
