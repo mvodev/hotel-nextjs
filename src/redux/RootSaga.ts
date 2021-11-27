0c834c7543a9a6264fd6f9d7412ae37a1ec03b4e
vitaliy <bakalov.vitaliy@gmail.com>
2021-11-27 19:37:54 +0700

    Changed way of launching child sagas

--- src/redux/RootSaga.ts
+++ src/redux/RootSaga.ts
@@ -1,9 +1,29 @@
-import { all } from '@redux-saga/core/effects';
+/* eslint-disable arrow-body-style */
+/* eslint-disable func-names */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { all, call, spawn } from '@redux-saga/core/effects';
+import { Saga } from 'redux-saga';
 
 function* RootSaga(): Saga<any[]> {
-  yield all([
-    /*sagas*/
-  ]);
+  const sagas: any[] = [
+    /* sagas */
+  ];
+
+  const retrySagas = yield sagas.map((saga) => {
+    return spawn(function* () {
+      while (true) {
+        try {
+          yield call(saga);
+          break;
+        } catch (e: unknown) {
+          console.log(e);
+        }
+      }
+    });
+  });
+
+  yield all(retrySagas);
 }
 
 export default RootSaga;
