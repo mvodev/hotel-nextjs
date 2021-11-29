d0df7fb102e92b97ead943c350f11b113b69243d
gamabuntat <schoshoev@yandex.ru>
2021-11-29 21:29:32 +0800

    refactore date text field

--- src/components/DateTextField/DateTextField.tsx
+++ src/components/DateTextField/DateTextField.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react';
 import MaskedInput from 'react-text-mask';
 
@@ -25,38 +24,6 @@ const DateTextField = (
       onBlur={onBlur}
       placeholder="ДД.ММ.ГГГГ"
       {...props}
-=======
-import { useState, useRef } from 'react';
-import InputMask from 'react-input-mask';
-
-import styles from './DateTextField.module.sass';
-
-const DateTextField = ({
-  placeholder = 'ДД.ММ.ГГГГ',
-}: {
-  placeholder?: string;
-}): JSX.Element => {
-  const [value, changeValue] = useState('');
-  const mask = useRef(placeholder.replace(/\p{L}/gu, '9'));
-
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    changeValue(e.target.value);
-    const isFilled = e.target.value.length === placeholder.length;
-    const isValidDate = Date.parse(e.target.value);
-    if (isFilled && isValidDate) {
-      // hihe
-    }
-  };
-
-  return (
-    <InputMask
-      className={styles.dateTextField}
-      mask={mask.current}
-      onChange={onChange}
-      value={value}
-      maskPlaceholder={null}
-      placeholder={placeholder}
->>>>>>> 64608ac (added date text field (masked text field))
     />
   );
 };
