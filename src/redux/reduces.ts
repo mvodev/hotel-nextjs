87f96e57f3992e171eabac1fd42cf5dc3c948c85
Виталий Бакалов <bakalov.vitaliy@gmail.com>
2021-12-02 15:44:32 +0000

    Resolved merge conflicts

--- src/redux/reduces.ts
+++ src/redux/reduces.ts
@@ -1,12 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
-import AuthenticationReducer from './Authentication/AuthenticationReducer';
 import dates from './Dates/DatesReducer';
 import prices from './Prices/PricesSlice';
 
 const reducers = combineReducers({
   dates,
   prices,
-  AuthenticationReducer,
 });
 
 type RootState = ReturnType<typeof reducers>;
