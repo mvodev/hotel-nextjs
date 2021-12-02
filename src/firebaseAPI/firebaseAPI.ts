65c583538fa33532bae3376ceb6b76d9be63a5c3
Konstantin Lozhkin <rizarid@mail.ru>
2021-12-02 15:09:26 +0500

    #49 added getFilters, addRoom, getRooms, firebaseFunctions

--- src/firebaseAPI/firebaseAPI.ts
+++ src/firebaseAPI/firebaseAPI.ts
@@ -1,4 +1,5 @@
 import { initializeApp, FirebaseApp } from "firebase/app";
+import { getFunctions, httpsCallable, Functions } from "firebase/functions";
 import { 
   getAuth, 
   Auth, 
@@ -7,8 +8,19 @@ import {
   UserCredential, 
   AuthError 
 } from "firebase/auth";
-import { getFirestore, Firestore, setDoc, doc } from 'firebase/firestore';
-import { UserDataType, UserType } from './Types';
+import {
+  getFirestore, 
+  Firestore, 
+  setDoc, 
+  addDoc, 
+  doc, 
+  getDoc, 
+  getDocs, 
+  collection, 
+  query,
+} from 'firebase/firestore';
+import { UserDataType, UserType, RoomType, FiltersAPIType, ReturnedRoomType } from './Types';
+import { FirebaseError } from "@firebase/util";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCKidrAaH_xAzc-QdlLrY-hkUHqJeijIA",
@@ -34,7 +46,7 @@ class FirebaseAPI {
     this.db = getFirestore(this.app);
   }
 
-  public signUp = async (userData: UserDataType): Promise<UserType | AuthError> => (
+  public signUp = async (userData: UserDataType): Promise<UserType | FirebaseError> => (
     createUserWithEmailAndPassword(this.auth, userData.email, userData.password)
       .then((userCredential: UserCredential) => {
         setDoc(doc(this.db, `userData/${userCredential.user.uid}`), {
@@ -49,17 +61,114 @@ class FirebaseAPI {
           email: userCredential.user.email
         }
       })
-      .catch((error: AuthError) => error)
+      .catch((error: FirebaseError) => error)
   );
 
-  public signIn = async (email: string, password: string): Promise<UserType | AuthError> => (
+  public signIn = async (email: string, password: string): Promise<UserType | FirebaseError> => (
     signInWithEmailAndPassword(this.auth, email, password)
-      .then((userCredential: UserCredential) => ({
-        uid: userCredential.user.uid, 
-        email: userCredential.user.email
-      }))
-      .catch((error: AuthError): AuthError => error)
-  )
+      .then((userCredential: UserCredential) => (
+        getDoc(doc(this.db, 'userData', userCredential.user.uid))
+          .then((userData) => ({
+            uid: userCredential.user.uid, 
+            email: userCredential.user.email,
+            ...userData.data()
+          }))
+      ))
+      .catch((error: FirebaseError): FirebaseError => error)
+  );
+
+  public getFilters = (): Promise<FiltersAPIType | FirebaseError> => (
+    getDoc(doc(this.db, 'filters', 'filters'))
+      .then((result) => (result.data() as FiltersAPIType))
+      .catch((error: FirebaseError): FirebaseError => error)
+  );
+
+  public addRoom = async (roomData: RoomType) => {
+    addDoc(collection(this.db, 'rooms'), roomData);
+  };
+
+  public getRooms = async (filters: FiltersAPIType, page: number, itemsOnPage: number) =>{
+    const roomsQuery = query(collection(this.db, "rooms"));
+    const rooms: ReturnedRoomType[] = [];
+    const selectionOfRooms = getDocs(roomsQuery)
+      .then((result) => result.forEach((item) => {
+        rooms.push(({roomID: item.id, ...item.data()} as ReturnedRoomType));
+      })).then(() => {
+        const filtredRooms = rooms.filter((item) => {
+          return this.filterRoom(item, filters);
+        });
+
+        return {
+          rooms: filtredRooms.slice(
+            ((itemsOnPage * page) - itemsOnPage), (itemsOnPage * page),
+          ),
+          esultsNumber: filtredRooms.length,
+          page,
+          pagesNumber: Math.ceil(filtredRooms.length / itemsOnPage),
+        };
+      });
+    return selectionOfRooms;
+  }
+
+  private filterRoom = (item: ReturnedRoomType, filters: FiltersAPIType) => {
+    const inPricesRange = (item.price >= filters.price[0]) &&
+      (item.price <= filters.price[1]);
+    if (!inPricesRange) return false;
+  
+    if (item.maxGuests < (filters.guests.adult + filters.guests.child)) {
+      return false;
+    }
+  
+    if (filters.dates[0] !== null) {
+      for (let i = 0; i < item.bookedDays.length; i += 1) {
+        const date = new Date(item.bookedDays[i].seconds * 1000);
+        if (
+          (date >= (filters.dates[0] as Date)) &&
+          (date <= (filters.dates[1] as Date))
+        ) return false;
+      }
+    }
+  
+    if (item.bedrooms < filters.conveniences.bedrooms.value) return false;
+    if (item.beds < filters.conveniences.beds.value) return false;
+    if (item.bathrooms < filters.conveniences.bathrooms.value) return false;
+  
+    if (filters.rules.maySmoking.checked && !item.maySmoking) return false;
+    if (filters.rules.mayWithPets.checked && !item.mayWithPets) return false;
+    if (filters.rules.mayInviteGuests.checked && !item.mayInviteGuests) {
+      return false;
+    }
+  
+    if (filters.availability.wideСorridor.checked && !item.wideСorridor) {
+      return false;
+    }
+    if (
+      filters.availability.assistantForDisabled.checked &&
+        !item.assistantForDisabled
+    ) return false;
+  
+    if (
+      filters.additionalConvenience.haveBreakfast.checked && !item.haveBreakfast
+    ) return false;
+    if (filters.additionalConvenience.haveCrib.checked && !item.haveCrib) {
+      return false;
+    }
+    if (filters.additionalConvenience.haveDesk.checked && !item.haveDesk) {
+      return false;
+    }
+    if (
+      filters.additionalConvenience.haveFeedingChair.checked &&
+        !item.haveFeedingChair
+    ) return false;
+    if (filters.additionalConvenience.haveShampoo.checked && !item.haveShampoo) {
+      return false;
+    }
+    if (filters.additionalConvenience.haveTV.checked && !item.haveTV) {
+      return false;
+    }
+  
+    return true;
+  };
 }
 
 const firebaseAPI = new FirebaseAPI();
