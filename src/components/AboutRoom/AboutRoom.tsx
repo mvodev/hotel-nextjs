79b185068c0dd3223faff8d4990df2fca0845902
vitaliy <bakalov.vitaliy@gmail.com>
2021-12-09 14:39:12 +0700

    AboutRoom redesigned for redux

--- src/components/AboutRoom/AboutRoom.tsx
+++ src/components/AboutRoom/AboutRoom.tsx
@@ -1,26 +1,33 @@
-import styles from './AboutRoom.module.sass';
-import type { TypeAboutRoomProps } from './Types';
-import defaultSections from './DefaultProps';
+import { useSelector } from 'react-redux';
+import { AppState } from 'src/redux/Store';
 
-const AboutRoom = ({
-  sections = defaultSections,
-}: TypeAboutRoomProps): JSX.Element => (
-  <div className={styles.aboutRoom}>
-    <h2 className={styles.title}>Сведения о номере</h2>
-    <ul className={styles.container}>
-      {sections.map(({ icon, title, description }) => (
-        <li className={styles.section} key={icon}>
-          <img
-            src={`/images/${icon}`}
-            alt="room service icon"
-            className={styles.sectionIcon}
-          />
-          <h3 className={styles.sectionTitle}>{title}</h3>
-          <span className={styles.sectionDescription}>{description}</span>
-        </li>
-      ))}
-    </ul>
-  </div>
-);
+import styles from './AboutRoom.module.sass';
+
+const AboutRoom = (): JSX.Element | null => {
+  const aboutBlocks = useSelector(
+    (state: AppState) => state.CurrentRoom.information
+  );
+
+  const aboutBlock = (
+    <div className={styles.aboutRoom}>
+      <h2 className={styles.title}>Сведения о номере</h2>
+      <ul className={styles.container}>
+        {aboutBlocks.map(({ icon, title, description }) => (
+          <li className={styles.section} key={icon}>
+            <img
+              src={`/images/${icon}`}
+              alt="room service icon"
+              className={styles.sectionIcon}
+            />
+            <h3 className={styles.sectionTitle}>{title}</h3>
+            <span className={styles.sectionDescription}>{description}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+
+  return aboutBlocks.length > 0 ? aboutBlock : null;
+};
 
 export default AboutRoom;
