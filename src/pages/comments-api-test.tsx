22eccd090c67845a779d75917ea82ca3b2292b67
Vadim Manushin <vadimmanushin@yandex.ru>
2021-12-12 17:21:39 +0300

    test page added

--- src/pages/comments-api-test.tsx
+++ src/pages/comments-api-test.tsx
@@ -4,8 +4,8 @@ import type { ReactElement } from 'react';
 import firebaseAPI from '../firebaseAPI/firebaseAPI';
 
 const CommentTest = (): ReactElement => {
-  // console.log(firebaseAPI.getCommentsByRoomID('94uSjhZzhOdYdPaccnlL'));
-  // console.log(firebaseAPI.getCommentsByUID('DdXyxubLACN6XDmWIpGwhdYMdsk1'));
+  console.log(firebaseAPI.getCommentsByRoomID('94uSjhZzhOdYdPaccnlL'));
+  console.log(firebaseAPI.getCommentsByUID('DdXyxubLACN6XDmWIpGwhdYMdsk1'));
   console.log(firebaseAPI.getImpressions('0L36vxMETKuBYt3i8BqU'));
   return <span />;
 }
