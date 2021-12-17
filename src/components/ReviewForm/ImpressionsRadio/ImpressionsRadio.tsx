67951b9d7ce09f44a96e6652aa00857edb8027d0
Vitalij Bakalov <bakalov.vitaliy@gmail.com>
2021-12-17 12:31:54 +0700

    Added ImpressionsRadio component

---
+++ src/components/ReviewForm/ImpressionsRadio/ImpressionsRadio.tsx
@@ -0,0 +1,29 @@
+import styles from './ImpressionsRadio.module.scss';
+import Values from './Values';
+
+const ImpressionsRadio = (): React.ReactElement => {
+  const radios = Values.map((radio) => {
+    return (
+      <label
+        className={[
+          styles.label,
+          styles[
+            `label${radio.key.charAt(0).toUpperCase()}${radio.key.slice(1)}`
+          ],
+        ].join(' ')}
+      >
+        {radio.value}
+        <input
+          className={styles.radio}
+          type='radio'
+          name='impressions'
+          value={radio.key}
+        />
+      </label>
+    );
+  });
+
+  return <div className={styles.impressionsRadio}>{radios}</div>;
+};
+
+export default ImpressionsRadio;
--- src/components/ReviewForm/ReviewForm.tsx
+++ src/components/ReviewForm/ReviewForm.tsx
@@ -1,5 +1,6 @@
 import styles from './ReviewForm.module.scss';
 import Button from '../Button/Button';
+import ImpressionsRadio from './ImpressionsRadio/ImpressionsRadio';
 
 const ReviewForm = (): React.ReactElement => {
   return (
@@ -7,6 +8,7 @@ const ReviewForm = (): React.ReactElement => {
       <h2 className={styles.title}>Оставьте свой отзыв об этом номере</h2>
       <form className={styles.form}>
         <textarea className={styles.textarea} placeholder='Ваш отзыв' />
+        <ImpressionsRadio />
         <div className={styles.submitContainer}>
           <Button type='submit' theme='filled' text='Оставить отзыв' />
         </div>
