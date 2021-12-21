6f97b99e06c046de17ca8fa435fb74239b5db4d2
gamabuntat <schoshoev@yandex.ru>
2021-12-21 18:56:55 +0800

    my room page fix NaN value

--- src/pages/myRooms.tsx
+++ src/pages/myRooms.tsx
@@ -47,7 +47,7 @@ const MyRooms = (): JSX.Element => {
         rating: RecalculateRating(room.impressions),
         price: room.price,
         reviewsCount: Object.values(room.impressions).reduce(
-          (acc, v) => acc + v
+          (acc, v) => acc + (v || 0), 0
         ),
       }}
     />
